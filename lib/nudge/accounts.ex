ea83aa75786e0832df54dc030234ee336a7c8f11
Menubatenny <tenny1998@gmail.com>
2021-08-30 10:25:19 +0530

    fix format

--- lib/nudge/accounts.ex
+++ lib/nudge/accounts.ex
@@ -10,12 +10,12 @@ defmodule Nudge.Accounts do
 
   @doc """
   Returns the list of users.
-  
+
   ## Examples
-  
+
       iex> list_users()
       [%User{}, ...]
-  
+
   """
   def list_user_sites(query, current_user_id) do
     from site in query,
@@ -28,17 +28,17 @@ defmodule Nudge.Accounts do
 
   @doc """
   Gets a single user.
-  
+
   Raises `Ecto.NoResultsError` if the User does not exist.
-  
+
   ## Examples
-  
+
       iex> get_user!(123)
       %User{}
-  
+
       iex> get_user!(456)
       ** (Ecto.NoResultsError)
-  
+
   """
   def get_user!(id), do: Repo.get!(User, id)
 
@@ -52,15 +52,15 @@ defmodule Nudge.Accounts do
 
   @doc """
   Creates a user.
-  
+
   ## Examples
-  
+
       iex> create_user(%{field: value})
       {:ok, %User{}}
-  
+
       iex> create_user(%{field: bad_value})
       {:error, %Ecto.Changeset{}}
-  
+
   """
   def create_user(attrs \\ %{}) do
     %User{}
@@ -70,15 +70,15 @@ defmodule Nudge.Accounts do
 
   @doc """
   Updates a user.
-  
+
   ## Examples
-  
+
       iex> update_user(user, %{field: new_value})
       {:ok, %User{}}
-  
+
       iex> update_user(user, %{field: bad_value})
       {:error, %Ecto.Changeset{}}
-  
+
   """
   def update_user(%User{} = user, attrs) do
     user
@@ -88,15 +88,15 @@ defmodule Nudge.Accounts do
 
   @doc """
   Deletes a user.
-  
+
   ## Examples
-  
+
       iex> delete_user(user)
       {:ok, %User{}}
-  
+
       iex> delete_user(user)
       {:error, %Ecto.Changeset{}}
-  
+
   """
   def delete_user(%User{} = user) do
     Repo.delete(user)
@@ -104,12 +104,12 @@ defmodule Nudge.Accounts do
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking user changes.
-  
+
   ## Examples
-  
+
       iex> change_user(user)
       %Ecto.Changeset{data: %User{}}
-  
+
   """
   def change_user(%User{} = user, attrs \\ %{}) do
     User.changeset(user, attrs)
@@ -130,12 +130,12 @@ defmodule Nudge.Accounts do
 
   @doc """
   Returns the list of sites.
-  
+
   ## Examples
-  
+
       iex> list_sites()
       [%Site{}, ...]
-  
+
   """
   def list_sites do
     Repo.all(Site)
@@ -143,31 +143,31 @@ defmodule Nudge.Accounts do
 
   @doc """
   Gets a single site.
-  
+
   Raises `Ecto.NoResultsError` if the Site does not exist.
-  
+
   ## Examples
-  
+
       iex> get_site!(123)
       %Site{}
-  
+
       iex> get_site!(456)
       ** (Ecto.NoResultsError)
-  
+
   """
   def get_site!(id), do: Repo.get!(Site, id)
 
   @doc """
   Creates a site.
-  
+
   ## Examples
-  
+
       iex> create_site(%{field: value})
       {:ok, %Site{}}
-  
+
       iex> create_site(%{field: bad_value})
       {:error, %Ecto.Changeset{}}
-  
+
   """
   def create_site(attrs \\ %{}) do
     %Site{}
@@ -177,15 +177,15 @@ defmodule Nudge.Accounts do
 
   @doc """
   Updates a site.
-  
+
   ## Examples
-  
+
       iex> update_site(site, %{field: new_value})
       {:ok, %Site{}}
-  
+
       iex> update_site(site, %{field: bad_value})
       {:error, %Ecto.Changeset{}}
-  
+
   """
   def update_site(%Site{} = site, attrs) do
     site
@@ -195,15 +195,15 @@ defmodule Nudge.Accounts do
 
   @doc """
   Deletes a site.
-  
+
   ## Examples
-  
+
       iex> delete_site(site)
       {:ok, %Site{}}
-  
+
       iex> delete_site(site)
       {:error, %Ecto.Changeset{}}
-  
+
   """
   def delete_site(%Site{} = site) do
     Repo.delete(site)
@@ -211,12 +211,12 @@ defmodule Nudge.Accounts do
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking site changes.
-  
+
   ## Examples
-  
+
       iex> change_site(site)
       %Ecto.Changeset{data: %Site{}}
-  
+
   """
   def change_site(%Site{} = site, attrs \\ %{}) do
     Site.changeset(site, attrs)
